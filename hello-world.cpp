4dbdc79d9c8f6ace861c686c59a24c8709c0a7b2
E Lahti <elahti14@gmail.com>
2025-04-01 19:47:21 -0500

    My spaghetti code in c++

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
+#include <string>
+using namespace std;
 int main() {
     
-    // my comment
-    std::cout << "Hello UTD" << std::endl;
+    int variableA = 10;
+    int variableB = 10;
+    int variableC = 10;
+    const int YAS = 12;
+    char arrayE[YAS] = {"Hello world"};
+    int count = 0;
+    if (variableA > 10 or variableA <= 10){
+        }if (variableB > 10 or variableB <= 10){
+            }if (variableC > 10 or variableC <= 10){
+                }if (count = 0){ cout << "Hello world" << endl; count++;}
+    else
+        for (int yus = 0; yus < YAS; yus++)
+            cout << arrayE[yus] << endl;
+        
+
+    
 
     return 0;
 }
